eda41922a69abef6b7857736ed1975400733a45a
Muhammed Furkan Kamer <32220015+furkankamer@users.noreply.github.com>
2019-10-27 21:53:53 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -3,17 +3,27 @@ import os
 import urllib.parse as up
 import psycopg2
 
+def postgres_test():
 
-conn = psycopg2.connect("dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'")
-cursor=conn.cursor()
-app = Flask(__name__)
-statement ="""CREATE TABLE if not exists Persons (
+    try:
+        conn = psycopg2.connect("dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'")
+        conn.close()
+        return True
+    except:
+        return 
+		
+
+if  postgres_test():
+	conn = psycopg2.connect("dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'")
+	cursor=conn.cursor()
+	app = Flask(__name__)
+	statement ="""CREATE TABLE if not exists Persons (
     UserName varchar(255),
     Password varchar(255),
     FirstName varchar(255),
     LastName varchar(255)
 );"""
-cursor.execute(statement)
+	cursor.execute(statement)
 
 @app.route("/")
 def home_page():
