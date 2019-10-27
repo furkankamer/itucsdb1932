abbf1cbbb4c3eae3627c8e4ba5590f5505254947
Muhammed Furkan Kamer <32220015+furkankamer@users.noreply.github.com>
2019-10-27 21:28:05 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
+import os
+import urllib.parse as up
+import psycopg2
 
 
+conn = psycopg2.connect("dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'")
+cursor=conn.cursor()
 app = Flask(__name__)
-
+statement ="""CREATE TABLE if not exists Persons (
+    UserName varchar(255),
+    Password varchar(255),
+    FirstName varchar(255),
+    LastName varchar(255)
+);"""
+cursor.execute(statement)
 
 @app.route("/")
 def home_page():
