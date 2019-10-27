8bfc6a1ddff43cb23808ead619c48042dbc387c3
Muhammed Furkan Kamer <32220015+furkankamer@users.noreply.github.com>
2019-10-27 23:59:09 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,31 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import psycopg2
+
+
+
+
+app = Flask(__name__)
 
 def postgres_test():
 
-    try:
-        conn = psycopg2.connect("dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'")
-        conn.close()
-        return True
-    except:
-        return 
-		
+	try:
+		conn = psycopg2.connect(
+			"dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'")
+		conn.close();
+		return True
+	except:
+		print('not connected')
+		return
 
 if  postgres_test():
-	conn = psycopg2.connect("dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'")
-	cursor=conn.cursor()
-	app = Flask(__name__)
-	statement ="""CREATE TABLE if not exists Persons (
-    UserName varchar(255),
-    Password varchar(255),
-    FirstName varchar(255),
-    LastName varchar(255)
-);"""
+	print('connected')
+	conn = psycopg2.connect(
+			"dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'")
+	cursor = conn.cursor()
+	statement = """CREATE TABLE if not exists Persons ( UserName varchar(255), Password varchar(255), FirstName varchar(255), LastName varchar(255) )"""
 	cursor.execute(statement)
+	conn.commit();
+	conn.close();
+
+    
+
 
 @app.route("/")
 def home_page():
     return render_template("homepage.html")
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+	conn = psycopg2.connect(
+			"dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'")
+	cursor = conn.cursor()
+	username = request.form['UserName']
+	password = request.form['Password']
+	firstname = request.form['FirstName']
+	lastname = request.form['LastName']
+	statement = """INSERT INTO Persons (UserName, Password, FirstName, LastName)
+VALUES ('%s', '%s', '%s', '%s');""" % (username,password,firstname,lastname)
+	cursor.execute(statement)
+	conn.commit();
+	conn.close();
+	return """Congratulations You have signed up!"""
+
+
+
 
 
 if __name__ == "__main__":
