d78025d94e9bc64162cf642e74954cbd6f5e5b07
furkankamer <furkankamer@gmail.com>
2019-12-12 16:33:45 +0300

    new tables added

--- dbinit.py
+++ dbinit.py
@@ -4,10 +4,54 @@ import sys
 import psycopg2 as dbapi2
 
 INIT_STATEMENTS = [
-    """CREATE TABLE if not exists Persons ( UserName varchar(255), Password varchar(255), FirstName varchar(255), 
-    LastName varchar(255) );""",
-    """CREATE TABLE if not exists Lectures ( Name varchar(255), Code varchar(255), Time varchar(255), 
-    Location varchar(255), Quota varchar(255) );""",
+    """CREATE TABLE if not exists Users ( id SERIAL UNIQUE, 
+	username varchar(255) NOT NULL UNIQUE, 
+	password varchar(255) NOT NULL,
+	title varchar(255) NOT NULL,
+	firstname varchar(255) NOT NULL, 
+    surname varchar(255) NOT NULL );""",
+    
+	"""CREATE TABLE if not exists Lectures ( id SERIAL UNIQUE, 
+	name varchar(255), 
+	crn DEC(3,0) NOT NULL UNIQUE, 
+	time varchar(255), 
+    weekday varchar(255), 
+	location int FOREIGN KEY REFERENCES Building(id) , 
+	quota DEC(3,0));""",
+	
+	"""CREATE TABLE if not exists Etudes   ( id SERIAL UNIQUE, 
+	subject varchar(255),
+	lectureid int FOREIGN KEY REFERENCES Lectures(id) , 
+	time datetime(0) 
+    day varchar(255), 
+	required_grade int, 
+	location varchar(255)FOREIGN KEY REFERENCES Buildings(id), 
+	quota DEC(3,0));""",
+	
+	"""CREATE TABLE if not exists Teachers ( id SERIAL UNIQUE, 
+	name varchar(255), 
+	crn DEC(3,0) NOT NULL UNIQUE, 
+	time varchar(255), 
+    day varchar(255), 
+	location varchar(255), 
+	quota DEC(3,0));""",
+	
+	"""CREATE TABLE if not exists Managers ( id SERIAL UNIQUE, 
+	name varchar(255), 
+	crn DEC(3,0) NOT NULL UNIQUE, 
+	time varchar(255), 
+    day varchar(255), 
+	location varchar(255), 
+	quota DEC(3,0));""",
+	
+	"""CREATE TABLE if not exists Students ( id SERIAL UNIQUE, 
+	name varchar(255) NOT NULL, 
+	surname varchar(255) NOT NULL, 
+	degree int NOT NULL, 
+	joindate DATE NOT NULL DEFAULT CURRENT_DATE, 
+	grade float,
+	userid int NOT NULL,
+	FOREIGN KEY (userid) REFERENCES Users(id));""",
 ]
 
 
