ebd3a112ddd1212f1488c20ce22aad93a6fec127
furkankamer <furkankamer@gmail.com>
2019-12-13 16:09:29 +0300

    sign up revised and database corrections

--- server.py
+++ server.py
@@ -24,11 +24,23 @@ def sign_in():
 
 @app.route("/lectures")
 def lectures():
-    names = ["Physics", "Biology", "Chemistry"]
-    deneme1 = """"""
-    for i in names:
-        deneme1 += """<option value="%s">%s</option>""" % (i, i)
-    return render_template("lectures.html", deneme=deneme1, deneme2="""""")
+	names = ["Physics", "Biology", "Chemistry"]
+	days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+	times = ["9:30", "11:30", "13:30", "15:30"]
+	locations = ["MED A34", "EHB 2101", "MDB 105"]
+	branches = """"""
+	day = """"""
+	time = """"""
+	location = """"""
+	for i in names:
+		branches += """<option value="%s">%s</option>""" % (i, i)
+	for i in days:
+		day += """<option value="%s">%s</option>""" % (i, i)
+	for i in times:
+		time += """<option value="%s">%s</option>""" % (i, i)
+	for i in locations:
+		location += """<option value="%s">%s</option>""" % (i, i)
+	return render_template("lectures.html", branchnames=branches,day=day,time=time,location=location)
 
 
 @app.route("/schedule")
@@ -44,40 +56,43 @@ def lectureregistry():
         deneme1 += """<option value="%s">%s</option>""" % (i, i)
     if request.method == "POST":
         car_brand = request.form.get("cars", None)
-    return render_template("lectures.html", deneme=deneme1, deneme2=car_brand)
+    return render_template("lectures.html", deneme=deneme1)
 
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    if request.form["btn"] == "Sign Up":
-        username = request.form['UserName']
-        password = pbkdf2_sha256.hash(request.form['Password'])
-        firstname = request.form['FirstName']
-        lastname = request.form['LastName']
-        statement = """INSERT INTO Persons (UserName, Password, FirstName, LastName) 
-        VALUES ('%s', '%s', '%s', '%s');""" % (username, password, firstname, lastname)
-        with psycopg2.connect(
-                """dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' 
-                password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'""") as connection:
-            with connection.cursor() as cursor:
-                cursor.execute(statement)
-        return render_template("homepage.html")
-    elif request.form["btn"] == "Sign In":
-        username = request.form['UserName']
-        password = request.form['Password']
-        statement = """SELECT password from Persons WHERE username = '%s'""" % (username,)
-        with psycopg2.connect(
-                """dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' 
-                password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'""") as connection:
-            with connection.cursor() as cursor:
-                cursor.execute(statement)
-                password1 = cursor.fetchone()[0]
-                if pbkdf2_sha256.verify(password, password1):
-                    return render_template("loggedin.html")
-                else:
-                    return """False information! Please try again"""
+	if request.form["btn"] == "Sign Up":
+		username = request.form['UserName']
+		password = pbkdf2_sha256.hash(request.form['Password'])
+		firstname = request.form['FirstName']
+		lastname = request.form['LastName']
+		title = request.form.getlist('title')
+		title = title[0]
+		print(title)
+		statement = """INSERT INTO Users (username, password, title, name, surname) 
+		VALUES ('%s', '%s', '%s', '%s', '%s');""" % (username, password, title, firstname, lastname)
+		with psycopg2.connect(
+				"""dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' 
+				password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'""") as connection:
+			with connection.cursor() as cursor:
+				cursor.execute(statement)
+		return render_template("homepage.html")
+	elif request.form["btn"] == "Sign In":
+		username = request.form['UserName']
+		password = request.form['Password']
+		statement = """SELECT password from Users WHERE username = '%s'""" % (username,)
+		with psycopg2.connect(
+				"""dbname='lsgowduy' user='lsgowduy' host='salt.db.elephantsql.com' 
+				password='FbiQok5ytKXzEjdU7MbH46l5AWJbKf3I'""") as connection:
+			with connection.cursor() as cursor:
+				cursor.execute(statement)
+				password1 = cursor.fetchone()[0]
+				if pbkdf2_sha256.verify(password, password1):
+					return render_template("loggedin.html")
+				else:
+					return """False information! Please try again"""
 
-    return """nothing"""
+	return """nothing"""
 
 
 if __name__ == "__main__":
